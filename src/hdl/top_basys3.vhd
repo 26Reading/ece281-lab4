9033f36fe3be06a9acfbe4e9950462f4715d240f
26Reading <c26cian.reading@afacademy.af.edu>
2024-04-16 18:59:56 -0600

    Revert "Basic Functionality in top_basys3"
    This reverts commit 761cb71dd4736394622f0cab4362cb9c7e11e84d.

--- src/hdl/top_basys3.vhd
+++ src/hdl/top_basys3.vhd
@@ -92,104 +92,21 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
   
 	-- declare components and signals
-	component elevator_controller_fsm is
-        Port ( i_clk     : in  STD_LOGIC;
-               i_reset   : in  STD_LOGIC;
-               i_stop    : in  STD_LOGIC;
-               i_up_down : in  STD_LOGIC;
-               o_floor   : out STD_LOGIC_VECTOR (3 downto 0)           
-             );
-    end component elevator_controller_fsm;
 
-   
-
-
-    component clock_divider is
-	generic ( constant k_DIV : natural := 2	); -- How many clk cycles until slow clock toggles
-											   -- Effectively, you divide the clk double this 
-											   -- number (e.g., k_DIV := 2 --> clock divider of 4)
-	port ( 	i_clk    : in std_logic;
-			i_reset  : in std_logic;		   -- asynchronous
-			o_clk    : out std_logic		   -- divided (slow) clock
-	);
-    end component clock_divider;
-
-    
-
-    
-    component sevenSegDecoder is
-    Port ( i_D : in STD_LOGIC_VECTOR (3 downto 0);
-           o_S : out STD_LOGIC_VECTOR (6 downto 0));
-    end component sevenSegDecoder;
   
-  
-  
-    component TDM4 is
-        generic ( constant k_WIDTH : natural  := 4); -- bits in input and output
-        Port ( i_clk        : in  STD_LOGIC;
-               i_reset        : in  STD_LOGIC; -- asynchronous
-               i_D3         : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
-               i_D2         : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
-               i_D1         : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
-               i_D0         : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
-               o_data        : out STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
-               o_sel        : out STD_LOGIC_VECTOR (3 downto 0)    -- selected data line (one-cold)
-        );
-    end component TDM4;
-    
-    
-    signal w_floor : std_logic_vector(3 downto 0) := "0010";
-    signal w_clk : std_logic;
- 
-  
-    
-    
-    
-    
 begin
 	-- PORT MAPS ----------------------------------------
-    clkdiv_inst : clock_divider 		--instantiation of clock_divider to take 
-    generic map ( k_DIV => 2500000 ) -- 2 Hz clock from 100 MHz
-    port map (                          
-       i_clk   => clk,
-       i_reset => btnL or btnU,
-       o_clk   => w_clk
-       );
-            
-	elevator_controller_fsm_init : elevator_controller_fsm
-	port map ( 
-	   i_clk => w_clk,
-	   i_reset => btnR or btnU,
-	   i_stop => sw(15),
-	   i_up_down => sw(0),
-	   o_floor => w_floor
-	   );
-	   
-	  sevenSegDecoder_fsm_init : sevenSegDecoder
-	  port map (
-	   i_D => w_floor,
-	   o_S => seg(6 downto 0)
-	  );
-	   
-	   
-	--TDM4_inst: TDM4
-	  -- port map (
-	    --   i_clk => w_clk,
-	      -- i_reset => btnL or btnU,
-	       --i_D
+
+	
+	
 	-- CONCURRENT STATEMENTS ----------------------------
 	
 	-- LED 15 gets the FSM slow clock signal. The rest are grounded.
-	led(14 downto 0) <= (others => '0');
-	led(15) <= w_clk;
+	
 
 	-- leave unused switches UNCONNECTED. Ignore any warnings this causes.
 	
-	
 	-- wire up active-low 7SD anodes (an) as required
-	an(2) <= '0';
 	-- Tie any unused anodes to power ('1') to keep them off
-	an(3) <= '1';
-	an(1) <= '1';
-	an(0) <= '1';
+	
 end top_basys3_arch;
